5ef2743cdcf79a8bb280e7284855c70cfea70b93
2024233250-KaichenGong <gongkch2024@shanghaitech.edu.cn>
2024-12-01 02:04:00 +0800

    1. 读取时序有问题 2. 写入不在文件上

--- projects/lab3/vsrc/src/im2col.v
+++ projects/lab3/vsrc/src/im2col.v
@@ -23,6 +23,7 @@ module im2col #(
     // Internal registers and signals
     reg [ADDR_WIDTH-1:0] addr_rd_reg;
     reg [ADDR_WIDTH-1:0] addr_wr_reg;
+    reg [DATA_WIDTH-1:0] data_wr_reg;
     reg [3:0] state;
     reg [7:0] row_counter;
     reg [7:0] col_counter;
@@ -40,10 +41,15 @@ module im2col #(
     wire[7:0] padding = is3x3 ? 8'b00000001 : 8'b00000000;
     // reg [DATA_WIDTH] mat [(1+IMG_H) * (1+IMG_W) * IMG_C];
     reg [DATA_WIDTH-1:0] mat [(1+IMG_H) * (1+IMG_W) * IMG_C - 1];
+    reg [DATA_WIDTH-1:0] transform [(FILTER_SIZE * FILTER_SIZE * IMG_C) * (IMG_H * IMG_W)];
     reg [7:0] mat_idx;
+    wire [7:0] idx;
+                    assign idx = row_counter * (FILTER_SIZE * FILTER_SIZE * IMG_C) + col_counter;
     assign mem_wr_en = (state == WRITE);
 
     assign addr_rd = addr_rd_reg;
+    assign addr_wr = addr_wr_reg;
+    assign data_wr = data_wr_reg;
 
 
     // read one data at a time
@@ -89,22 +95,60 @@ module im2col #(
                             channel_counter <= channel_counter + 1;
                         end
                         if (addr_rd_reg + 1 == IMG_C * IMG_H * IMG_W) begin
+                            row_counter <= 0;
+                            channel_counter <= 0;
+                            col_counter <= 0;
+                            addr_wr_reg <= IM2COL_BASE;
+                            mat_idx <= 0;
+                            // Transform the data into column-major format
+                            for (int h = 0; h < IMG_H; ++h) begin
+                                for (int w = 0; w < IMG_W; ++w) begin
+                                // For each unit in the output matrix, it results from a convolution, which we need to converts into a dot product of two vector
+                                    int row = h * IMG_W + w;
+                                    // one row of transformed matrix represents C reception fields flatterned and concatenate together 
+                                    for (int c = 0; c < IMG_C; ++c) begin
+                                        for (int fh = 0; fh < FILTER_SIZE; ++fh) begin
+                                            for (int fw = 0; fw < FILTER_SIZE; ++fw) begin
+                                                int ih = h + fh;
+                                                int iw = w + fw;
+                                                transform
+                                                [
+                                                 row * (FILTER_SIZE * FILTER_SIZE * IMG_C)
+                                                 + c * FILTER_SIZE * FILTER_SIZE + fh * FILTER_SIZE + fw
+                                                ]
+                                                = mat[c * (IMG_H * IMG_W) + ih * IMG_W + iw];
+                                            end
+                                        end
+                                    end
+                                end
+                            end
                             state <= WRITE;
                         end else begin
                             addr_rd_reg <=  IMG_BASE + addr_rd_reg + 1;
                         end
                     end
-               WRITE: 
-               begin
-               end
+               WRITE:
+                begin
+                    if (row_counter + 1 == IMG_H * IMG_W) begin
+                        row_counter <= 0;
+                        if (col_counter + 1 == FILTER_SIZE * FILTER_SIZE * IMG_C) begin
+                        end else begin
+                            col_counter <= col_counter + 1;
+                        end
+                    end else begin
+                        row_counter <= row_counter + 1;
+                    end
+                    data_wr_reg <= transform[idx[3:0]];
+                    if (addr_wr_reg + 1 == FILTER_SIZE * FILTER_SIZE * IMG_C * IMG_H * IMG_W + IM2COL_BASE) begin
+                        state <= DONE;
+                    end else begin
+                        addr_wr_reg <= addr_wr_reg + 1;
+                    end
+                end
                DONE: done <= 1;
                default: state <= IDLE;
             endcase
-                
         end
     end
-
-
-    
 
 endmodule
