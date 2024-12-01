906985316320be318db6a7ac5ca0254e37596d9d
2024233250-KaichenGong <gongkch2024@shanghaitech.edu.cn>
2024-12-01 20:10:44 +0800

    add systolic array

--- projects/lab3/vsrc/src/systolic_array.v
+++ projects/lab3/vsrc/src/systolic_array.v
@@ -14,7 +14,40 @@ module systolic_array #(
     output reg [DATA_WIDTH*M*K-1:0] Y,  // Output result matrix Y
     output reg done  // Signal indicating the completion of the computation
 );
+// Internal signal to track the progress
+reg [31:0] cnt ;
 
-assign done = 1;
+// Generate the PE array
+genvar i, j;
+generate
+    for (i = 0; i < M; i = i + 1) begin : pe_row
+        for (j = 0; j < K; j = j + 1) begin : pe_column
+            // Instantiate the PE
+            pe #(.DATA_WIDTH(DATA_WIDTH)) pe_inst (
+                .clk(clk),
+                .rst(~rst_n),  // Invert the reset signal as it is active low
+                .x_in(X[(i+1)*DATA_WIDTH-1:i*DATA_WIDTH]),
+                .w_in(W[(j+1)*DATA_WIDTH-1:j*DATA_WIDTH]),
+                .x_out(),
+                .w_out(),
+                .y_out(Y[(i*K+j+1)*DATA_WIDTH-1:(i*K+j)*DATA_WIDTH])
+            );
+        end
+    end
+endgenerate
+
+always @(posedge clk) begin
+    if (!rst_n) begin
+        cnt <= 0;
+        done <= 0;
+    end else begin
+        cnt <= cnt  + 1;
+        if (cnt >= (M * K)) begin
+            done <= 1;
+        end else begin
+            done <= 0;
+        end
+    end
+end
 
 endmodule
