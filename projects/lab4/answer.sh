ed878b0251697bfc96b3da5ef4ca84af99b4c099
trace1279 <gongkch2024@shanghaitech.edu.cn>
2024-12-24 09:22:46 +0800

    recommandation

--- projects/lab4/answer.sh
+++ projects/lab4/answer.sh
@@ -15,3 +15,5 @@ make run IMG=task3_2 ISSUE_NUM=3
 # Give your suggestion here (中英文皆可)
 # =====================================
 # 可以给出你对这个实验的评价或建议
+# 1. It would be helpful if the homework includes brief explanations for each module port to clarify their functionality and intended usage. This would ensure a better understanding of the design and facilitate implementation. For example, specifying the purpose of input and output signals, as well as their data width or behavior, can provide clarity.
+# 2. The test case is not handy for debugging, adding difftest would definatly make our life eaiser.
